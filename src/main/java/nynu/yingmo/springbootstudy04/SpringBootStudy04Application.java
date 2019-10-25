046b335eae08e1e2bf4c8d1023ba104a1a84db5a
xiaohai <1263774090@qq.com>
2019-10-25 21:16:33 +0800

    init

---
+++ src/main/java/nynu/yingmo/springbootstudy04/SpringBootStudy04Application.java
@@ -0,0 +1,13 @@
+package nynu.yingmo.springbootstudy04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStudy04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStudy04Application.class, args);
+	}
+
+}
---
+++ src/test/java/nynu/yingmo/springbootstudy04/SpringBootStudy04ApplicationTests.java
@@ -0,0 +1,13 @@
+package nynu.yingmo.springbootstudy04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStudy04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
